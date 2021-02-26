72fc69df8deb1cad7cca5a4af0de047e88b5685a
Kevin Fraga <kevinciolac@gmail.com>
2021-02-26 19:45:24 -0300

    Ex.1 - now properly working

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -7,16 +7,28 @@ class Pokedex extends React.Component {
     this.state = {
       showcase: 0,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState((anterior, _props) => ({
+      showcase: anterior.showcase < this.props.pokemons.length - 1 ? anterior.showcase + 1 : 0,
+    }));
   }
 
   render() {
     return (
-      <div className="pokedex">
-        {this.props.pokemons
-          .filter((pokemon) => this.props.pokemons.indexOf(pokemon) === this.state.showcase)
-          .map((pokemon) => (
-            <Pokemon key={pokemon.id} pokemon={pokemon} />
-          ))}
+      <div>
+        <div className="pokedex">
+          {this.props.pokemons
+            .filter((pokemon) => this.props.pokemons.indexOf(pokemon) === this.state.showcase)
+            .map((pokemon) => (
+              <Pokemon key={pokemon.id} pokemon={pokemon} />
+            ))}
+        </div>
+        <div>
+          <button onClick={this.handleClick} >Próximo Pokemon</button>
+        </div>
       </div>
     );
   }
