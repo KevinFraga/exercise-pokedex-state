472ec2adb6e52039209a67acc7ba1a22e14cc689
Kevin Fraga <kevinciolac@gmail.com>
2021-02-26 19:08:03 -0300

    Ex.1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showcase: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div className="pokedex">
+        {this.props.pokemons
+          .filter((pokemon) => this.props.pokemons.indexOf(pokemon) === this.state.showcase)
+          .map((pokemon) => (
+            <Pokemon key={pokemon.id} pokemon={pokemon} />
+          ))}
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
