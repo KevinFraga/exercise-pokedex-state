94bbc01b04a01c1e92b9e031f7e3a2418d400e79
Kevin Fraga <kevinciolac@gmail.com>
2021-02-26 20:31:26 -0300

    Ex.2

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -6,14 +6,36 @@ class Pokedex extends React.Component {
     super(props);
     this.state = {
       showcase: 0,
+      type: 'Fire',
     };
     this.handleClick = this.handleClick.bind(this);
+    this.fireClick = this.fireClick.bind(this);
+    this.psychicClick = this.psychicClick.bind(this);
   }
 
   handleClick = () => {
     this.setState((anterior, _props) => ({
-      showcase: anterior.showcase < this.props.pokemons.length - 1 ? anterior.showcase + 1 : 0,
+      showcase:
+        anterior.showcase <
+        this.props.pokemons.filter(
+          (pokemon) => pokemon.type === this.state.type
+        ).length -
+          1
+          ? anterior.showcase + 1
+          : 0,
     }));
+  };
+
+  fireClick() {
+    this.setState({
+      type: 'Fire',
+    });
+  }
+
+  psychicClick() {
+    this.setState({
+      type: 'Psychic',
+    });
   }
 
   render() {
@@ -21,13 +43,23 @@ class Pokedex extends React.Component {
       <div>
         <div className="pokedex">
           {this.props.pokemons
-            .filter((pokemon) => this.props.pokemons.indexOf(pokemon) === this.state.showcase)
+            .filter((pokemon) => pokemon.type === this.state.type)
+            .filter(
+              (pokemon) =>
+                this.props.pokemons
+                  .filter((pokemon) => pokemon.type === this.state.type)
+                  .indexOf(pokemon) === this.state.showcase
+            )
             .map((pokemon) => (
               <Pokemon key={pokemon.id} pokemon={pokemon} />
             ))}
         </div>
         <div>
-          <button onClick={this.handleClick} >Próximo Pokemon</button>
+          <button onClick={this.handleClick}>Próximo Pokemon</button>
+        </div>
+        <div>
+          <button onClick={this.fireClick}>Fire</button>
+          <button onClick={this.psychicClick}>Psychic</button>
         </div>
       </div>
     );
